5986d2d3d8add469d5b01952f1d6b99af3200fe2
billjohnston <billjohnston4@gmail.com>
2016-02-16 16:45:56 -0500

    Don't need thunk anymore

--- react/main.js
+++ react/main.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { createStore, applyMiddleware  } from 'redux'
-import thunkMiddleware from 'redux-thunk'
 import { Provider } from 'react-redux'
 import rootReducer from './reducers/reducer'
 import App from './containers/app'
@@ -9,11 +8,7 @@ import {StyleRoot} from 'radium'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 injectTapEventPlugin()
 
-const createStoreWithMiddleware = applyMiddleware(
-    thunkMiddleware
-)(createStore)
-
-const store = createStoreWithMiddleware(rootReducer)
+const store = createStore(rootReducer)
 
 render(
     <StyleRoot className='flex layout-column layout-fill'>
